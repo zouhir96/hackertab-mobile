7c69de3f9b6ad0ac67773918b5f71c3414443ff6
zouhir <rajdaouizouhir.pro@gmail.com>
2024-08-11 02:56:47 +0200

    feat: Add kotlinx-datetime dependencies.

--- build-logic/convention/src/main/kotlin/com/zrcoding/convention/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/zrcoding/convention/KotlinAndroid.kt
@@ -5,6 +5,7 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
@@ -26,6 +27,9 @@ internal fun Project.configureKotlinAndroid(
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
+        }
+        dependencies {
+            add("implementation", versionCatalog().findLibrary("kotlinx.datetime").get())
         }
     }
 
