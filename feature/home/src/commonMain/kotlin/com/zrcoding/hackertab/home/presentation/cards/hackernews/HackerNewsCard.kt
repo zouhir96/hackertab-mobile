ae746bf2f6a15c1c7fe95cd0acbb3944b5432ffa
zouhir <rajdaouizouhir.pro@gmail.com>
2025-04-18 22:33:22 +0200

    feat: Use card abstraction for hackernews source.

---
+++ feature/home/src/commonMain/kotlin/com/zrcoding/hackertab/home/presentation/cards/hackernews/HackerNewsCard.kt
@@ -0,0 +1,87 @@
+package com.zrcoding.hackertab.home.presentation.cards.hackernews
+
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.zrcoding.hackertab.design.components.TextWithStartIcon
+import com.zrcoding.hackertab.design.components.icon
+import com.zrcoding.hackertab.design.resources.Res
+import com.zrcoding.hackertab.design.resources.comments
+import com.zrcoding.hackertab.design.resources.ic_comment
+import com.zrcoding.hackertab.design.resources.ic_ellipse
+import com.zrcoding.hackertab.design.resources.ic_time_24
+import com.zrcoding.hackertab.design.resources.score
+import com.zrcoding.hackertab.design.theme.Flamingo
+import com.zrcoding.hackertab.design.theme.HackertabTheme
+import com.zrcoding.hackertab.domain.models.HackerNews
+import com.zrcoding.hackertab.domain.models.Source
+import com.zrcoding.hackertab.home.presentation.cards.CardWithoutTopicFilterTemplate
+import com.zrcoding.hackertab.home.presentation.cards.SourceItemTemplate
+import com.zrcoding.hackertab.home.presentation.utils.timeAgo
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun HackerNewsCard(
+    modifier: Modifier = Modifier,
+    viewModel: HackerNewsCardViewModel = koinViewModel()
+) {
+    val viewState = viewModel.viewState.collectAsStateWithLifecycle().value
+    CardWithoutTopicFilterTemplate(
+        modifier = modifier,
+        cardUiState = viewState,
+        sourceName = Source.HACKER_NEWS.label,
+        sourceIcon = Source.HACKER_NEWS.icon,
+        cardItem = { model ->
+            HackerNewsItem(new = model)
+        },
+        onRetryBtnClick = viewModel::onRefresh
+    )
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun HackerNewsItem(new: HackerNews) {
+    SourceItemTemplate(
+        title = new.title,
+        primaryInfoSection = {
+            TextWithStartIcon(
+                text = stringResource(Res.string.score, new.score),
+                textColor = Flamingo,
+                icon = Res.drawable.ic_ellipse,
+                tint = Flamingo
+            )
+            TextWithStartIcon(
+                icon = Res.drawable.ic_time_24,
+                text = new.time.timeAgo()
+            )
+            TextWithStartIcon(
+                text = stringResource(Res.string.comments, new.descendants),
+                icon = Res.drawable.ic_comment
+            )
+        },
+        url = new.url,
+    )
+}
+
+@Preview()
+@Composable
+fun HackerNewsItemPreview() {
+    HackertabTheme {
+        HackerNewsItem(
+            new = HackerNews(
+                id = "",
+                title = "React is the best web framework ever React is the best web framework ever",
+                url = "https://www.google.com/#q=propriae",
+                time = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()),
+                descendants = 1234,
+                score = 1234
+            )
+        )
+    }
+}
